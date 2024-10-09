f45581816ad24c440ce906ba9995aab7e7e5c833
Costa05br <j.costa009@edu.pucrs.br>
2024-10-09 10:46:07 -0300

    Implementando os métodos da classe Pedido

--- Pedido.java
+++ Pedido.java
@@ -22,28 +22,53 @@ public class Pedido{
       this.status = StatusPedido.ABERTO;
       this.valorTotal = 0.0;
   }
-  
-    public double calculaTotal(int i){
-        double total = 0;
-       for (int i =0; i<= items.size()) {
 
-            if (1<5){
-                String teste = String.valueOf(Status.PENDENTE);
-
-            }
-                    double valor = items(i).get(i).getValor(i);
-                int quantidade = items(i).get(i).get;
-            total += valor * quantidade;
-                            i++;
-       }
-        return total;
-
+    public void adicionarItem(Item item) {
+        itens.add(item);
+        calcularValorTotal();
     }
 
+    public double calcularValorTotal() {
+        valorTotal = 0;
+        for (Item item : itens) {
+            valorTotal += item.calcularTotal();
+        }
+        return valorTotal;
+    }
+
+    public List<Item> getItens() {
+        return itens;
+    }
+
+    public Funcionario getFuncionarioSolicitante() {
+        return funcionarioSolicitante;
+    }
+
+    public StatusPedido getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusPedido status) {
+        this.status = status;
+    }
+
+    public Date getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(Date dataConclusao) {
+        this.dataConclusao = dataConclusao; // Método para definir a data de conclusão
+    }
+
+    public double getValorTotal() {
+        return valorTotal; // Método para obter o valor total
+    }
 
     public String getId() {
-      return String.valueOf(id);
+        return String.valueOf(id);
     }
 }
+
+
 
 

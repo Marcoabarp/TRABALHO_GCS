28b07f035ab0686aaad73cef733e4c6b26dc6873
Costa05br <j.costa009@edu.pucrs.br>
2024-10-09 09:40:52 -0300

    Fazendo altereções nas classes

--- Departamento.java
+++ Departamento.java
@@ -2,5 +2,35 @@ public class Departamento {
 
     private int id;
     private String nome;
+    private double limitePedido;
 
+    public Departamento(int id, String nome, double limitePedido) {
+        this.id = id;
+        this.nome = nome;
+        this.limitePedido = limitePedido;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getLimitePedido() {
+        return limitePedido;
+    }
+
+    public void setLimitePedido(double limitePedido) {
+        this.limitePedido = limitePedido;
+    }
 }

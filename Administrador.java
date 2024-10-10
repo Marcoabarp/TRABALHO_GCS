83ba7c3657398bb1f90625094ce9c3e34af2dd80
Marco Antônio B. F. de Almeida <139078307+Marcoabarp@users.noreply.github.com>
2024-10-10 13:10:34 -0300

    VisualizarDetalhesPedido ADD
    Adicionei o VisualizarDetalhesPedido

--- Administrador.java
+++ Administrador.java
@@ -55,6 +55,17 @@ public class Administrador extends Usuario {
         return pedidosDescricao;
     }
 
+    public void visualizarDetalhesPedido(Pedido pedido) {
+        System.out.println("Detalhes do Pedido " + pedido.getId() + ":");
+        System.out.println("Solicitante: " + pedido.getFuncionarioSolicitante().getNome());
+        System.out.println("Departamento: " + pedido.getFuncionarioSolicitante().getDepartamento().getNome());
+        System.out.println("Status: " + pedido.getStatus());
+        System.out.println("Valor Total: " + pedido.getValorTotal());
+        for (Item item : pedido.getItens()) {
+            System.out.println("- " + item.getDescricao() + ": " + item.getQuantidade() + " x " + item.getValorUnitario() + " = " + item.calcularTotal());
+        }
+    }
+
 
 
 }
